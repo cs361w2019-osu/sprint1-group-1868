2abece375e6012248e6ffb89ceed97af29af4472
HaoChong Pan <32114924+Joeking123@users.noreply.github.com>
2019-01-21 17:00:14 -0800

    Add files via upload

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -6,19 +6,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ship {
+	@JsonProperty private String kind;
+	@JsonProperty private ArrayList<Square> occupiedSquares;
+	@JsonProperty private int s_size;
 
-	@JsonProperty private List<Square> occupiedSquares;
 
 	public Ship() {
-		occupiedSquares = new ArrayList<>();
+		//occupiedSquares = new ArrayList<>();
+		this.kind = new String();
+
 	}
-	
+
+	public int s_size() {
+		return this.s_size;
+	}
+
+
+	public String kind(){
+		return this.kind;
+	}
+
 	public Ship(String kind) {
-		//TODO implement
+		this.kind = kind;
+		if(kind.equals("MINESWEEPER")){
+				this.s_size = 2;
+				occupiedSquares = new ArrayList<>();
+		}
+		else if (kind.equals("DESTROYER")){
+				this.s_size = 3;
+				occupiedSquares = new ArrayList<>(3);
+		}
+		else{
+				this.s_size = 4;
+				occupiedSquares = new ArrayList<>(4);
+
+		}
+		//occupiedSquares = new ArrayList<>();
 	}
 
 	public List<Square> getOccupiedSquares() {
-		//TODO implement
-		return null;
+		return occupiedSquares;
 	}
+	public void takespot(int x, char y , boolean isVertical){
+
+	}
+
 }
