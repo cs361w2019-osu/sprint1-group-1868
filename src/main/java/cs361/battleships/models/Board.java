e5de16cf2ab1f865560817150e7064e1a0aedd3e
HaoChong Pan <32114924+Joeking123@users.noreply.github.com>
2019-01-21 16:58:45 -0800

    Add files via upload

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -1,15 +1,31 @@
 package cs361.battleships.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-
+	@JsonProperty private List<Square> squares ;
+	@JsonProperty private List<Ship> ships;
+	//@JsonProperty private List<Result> attact_results;
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public Board() {
 		// TODO Implement
+
+		this.ships = new ArrayList<>();   //initail with 0 ships within it
+		/*for(int i = 0; i < 10; i++){
+			for(int j = 0 ; j < i ; j++){
+				this.squares[i][j] = new Square(x,y);
+				System.out.println(this.squares[i].get(j).getRow());
+				System.out.print(this.squares[i][j].getColumn());
+				y++;
+			}
+			x++;
+		}*/
+		//this.attact_results = new ArrayList<>();
 	}
 
 	/*
@@ -17,9 +33,23 @@ public class Board {
 	 */
 	public boolean placeShip(Ship ship, int x, char y, boolean isVertical) {
 		// TODO Implement
+
+		if(getShips().size()==0) {
+			 if(check_location(getShips().size(), x, y , isVertical)) {
+			 	ship.takespot(x,y,isVertical);
+				this.getShips().add(ship);
+				return true;
+			}
+		}
+
+
+
 		return false;
 	}
 
+	private boolean check_location(int size, int x, char y, boolean vertical){
+			return false;
+	}
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
@@ -30,11 +60,14 @@ public class Board {
 
 	public List<Ship> getShips() {
 		//TODO implement
-		return null;
+
+		return this.ships;
 	}
 
 	public void setShips(List<Ship> ships) {
+
 		//TODO implement
+		this.ships = ships;
 	}
 
 	public List<Result> getAttacks() {
