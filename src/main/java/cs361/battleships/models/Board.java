585c87eee08a66d9dff7e6d1c5c3e1ebc30fbf03
Shawn_huang <38021236+hunagyux@users.noreply.github.com>
2019-01-21 23:52:21 -0800

    Update Board.java

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -4,89 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-	private int miss;
-	private int hit;
-	private int unhit;
-	private int unknown;
-
+	@JsonProperty private List<Square> emptySquares;
+	@JsonProperty private List<Square> hitSquares;
+	@JsonProperty private List<Square> missedSquares;
+	@JsonProperty private List<Ship> placedShips;
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public Board() {
+		placedShips = new ArrayList<>();
 		// TODO Implement
-		miss = 0;
-		hit = 0;
-		unhit = 9;
-		unknown = 100;
 	}
 
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public boolean placeShip(Ship ship, int x, char y, boolean isVertical) {
+
 		// TODO Implement
-		int area = ship.getOccupiedSquares();
-		int yy = y;
-		yy -= 64;
-		if(isVertical){
-			if((x+area)>10 || (x-area)<0){
-				return false;
-			}
-			else{
-				return true;
-			}
-		}
-		else{
-			if((yy+area)>10 || (yy-area)<0){
-				return false;
-			}
-			else{
-				return true;
-			}
-		}
+		return false;
 	}
 
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public Result attack(int x, char y) {
-		//TODO Implement
 
+		//TODO Implement
 		return null;
 	}
 
 	public List<Ship> getShips() {
+		return this.placedShips;
 		//TODO implement
-
-
-		return null;
 	}
 
 	public void setShips(List<Ship> ships) {
+		this.placedShips = ships;
 		//TODO implement
 	}
 
 	public List<Result> getAttacks() {
-		//TODO implement
-		AtackStatus a = AtackStatus.HIT;
-		Result r = new Result();
 
-		return r.setResult(a);
+		//TODO implement
+		return null;
 	}
 
 	public void setAttacks(List<Result> attacks) {
+
 		//TODO implement
-		if(attacks.getResult() == "MISS"){
-			return false;
-		}
-		else if(attacks.getResult() == "HIT"){
-			return false;
-		}
-		else if(attacks.getResult() == "SUNK"){
-			return false;
-		}
-		else{
-			return true;
-		}
 	}
 }
