d087cae3199a3ba3b928db1ca750b333a59356f3
HaoChong Pan <32114924+Joeking123@users.noreply.github.com>
2019-02-23 21:07:06 -0800

    Add files via upload

--- src/test/java/cs361/battleships/models/BoardTest.java
+++ src/test/java/cs361/battleships/models/BoardTest.java
@@ -12,43 +12,78 @@ public class BoardTest {
     {
         Board board = new Board();
         Board board_a = new Board();
-        //enemy board error setting
+        //enemy board error setting --------------------------------------------------------------------
         assertFalse(board_a.placeShip(new Ship("MINESWEEPER"), 10, 'J', true));     //To check if the place ship will throw error if in invalid position
         assertFalse(board_a.placeShip(new Ship("BATTLESHIP"), 10, 'J', true));
         assertFalse(board_a.placeShip(new Ship("DESTROYER"), 10, 'J', true));
         assertFalse(board_a.placeShip(new Ship("MINESWEEPER"), 0, 'J', false));
         assertFalse(board_a.placeShip(new Ship("BATTLESHIP"), 0, 'J', false));
         assertFalse(board_a.placeShip(new Ship("DESTROYER"), 0, 'I', false));
-        //enemy board correct setting
+        //---------------------------------------------------------------------------------------------
+
+        //enemy board correct setting------------------------------------------------------------------
         assertTrue(board_a.placeShip(new Ship("MINESWEEPER"), 2, 'B', true));       //To check if the place ship will correct insert the ship
+        //error becuase square overlap
+        assertFalse(board_a.placeShip(new Ship("BATTLESHIP"), 2, 'B', false));
         assertTrue(board_a.placeShip(new Ship("BATTLESHIP"), 6, 'D', true));
-        assertTrue(board_a.placeShip(new Ship("DESTROYER"), 7, 'H', false));
-        //player board error setting
+        //error becasue place same ship again
+        assertFalse(board_a.placeShip(new Ship("MINESWEEPER"), 7, 'H', false));
+        assertTrue(board_a.placeShip(new Ship("DESTROYER"), 7, 'H', true));
+        //--------------------------------------------------------------------------------------------
+
+
+        //player board error setting----------------------------------------------------------------------
         assertFalse(board.placeShip(new Ship("MINESWEEPER"), 10, 'J', true));     //To check if the place ship will throw error if in invalid position
         assertFalse(board.placeShip(new Ship("BATTLESHIP"), 10, 'J', true));
         assertFalse(board.placeShip(new Ship("DESTROYER"), 10, 'J', true));
         assertFalse(board.placeShip(new Ship("MINESWEEPER"), 0, 'J', false));
         assertFalse(board.placeShip(new Ship("BATTLESHIP"), 0, 'J', false));
         assertFalse(board.placeShip(new Ship("DESTROYER"), 0, 'I', false));
-        //player board correct setting
-        assertTrue(board.placeShip(new Ship("MINESWEEPER"), 2, 'B', true));       //To check if the place ship will correct insert the ship
-        assertTrue(board.placeShip(new Ship("BATTLESHIP"), 6, 'D', true));
-        assertTrue(board.placeShip(new Ship("DESTROYER"), 7, 'H', false));
+        //------------------------------------------------------------------------------------------------
 
-        //Some miss hit result
+        //player board correct setting -------------------------------------------------------------------
+        assertTrue(board.placeShip(new Ship("MINESWEEPER"), 3, 'B', true));       //To check if the place ship will correct insert the ship
+        //error becuase square overlap
+        assertFalse(board.placeShip(new Ship("BATTLESHIP"), 3, 'B', false));
+        assertTrue(board.placeShip(new Ship("BATTLESHIP"), 5, 'D', true));
+        //error becasue place same ship again
+        assertFalse(board.placeShip(new Ship("MINESWEEPER"), 7, 'H', false));
+        assertTrue(board.placeShip(new Ship("DESTROYER"), 4, 'H', false));
+        //----------------------------------------------------------------------------------------------
+        //Some miss hit result on both board
+        assertTrue(board.attack(1,'B').getResult() == AtackStatus.MISS);
+        assertTrue(board_a.attack(7,'B').getResult() == AtackStatus.MISS);
+        assertTrue(board.attack(7,'I').getResult() == AtackStatus.MISS);
+        assertTrue(board_a.attack(5,'I').getResult() == AtackStatus.MISS);
+        assertTrue(board.attack(8,'B').getResult() == AtackStatus.MISS);
+
+        //player hit ship of enemy
+        assertTrue(board_a.attack(2,'B').getResult() == AtackStatus.HIT);
+        //enemy hit ship of player normally
+        assertTrue(board.attack(3,'B').getResult() == AtackStatus.HIT);
+        //player sunk ship of enemy
+        assertTrue(board_a.attack(1,'B').getResult() == AtackStatus.SUNK);
+        //enemy sunk ship of player
+        assertTrue(board.attack(2,'B').getResult() == AtackStatus.SUNK);
+
+        //player hit one captain quarter of enemy's ship
+        assertTrue(board_a.attack(7,'D').getResult() == AtackStatus.CAPTAIN);
+        assertTrue(board.attack(6,'D').getResult() == AtackStatus.CAPTAIN);
+        //player sunk one enemy'ship by only hitting twice captain quarter
+        assertTrue(board_a.attack(7,'D').getResult() == AtackStatus.SUNK);
+        //enemy sunk one player'ship by only hitting twice captain quarter
+        assertTrue(board.attack(6,'D').getResult() == AtackStatus.SUNK);
+
+        //enemy' another captain quarter got hit again
+        assertTrue(board_a.attack(7,'H').getResult() == AtackStatus.CAPTAIN);
+        assertTrue(board.attack(4,'H').getResult() == AtackStatus.MISS);
 
 
-        assertTrue(board.attack(2,'B').getResult() == AtackStatus.HIT);
-        /*assertTrue(board.attack(5,'C').getResult() == AtackStatus.HIT);      //To check if the shot result could record the hit status
-        assertTrue(board.attack(5,'D').getResult() == AtackStatus.HIT);
-        assertTrue(board.attack(5,'E').getResult() == AtackStatus.HIT);
-        assertTrue(board.attack(5,'F').getResult() == AtackStatus.SUNK);
+        //This is last ship of enemy being hit by player. Game over
+        assertTrue(board_a.attack(7,'H').getResult() == AtackStatus.SURRENDER);
 
-        assertTrue(board.attack(2,'E').getResult() == AtackStatus.HIT);
-        assertTrue(board.attack(2,'F').getResult() == AtackStatus.HIT);
-        assertTrue(board.attack(2,'G').getResult() == AtackStatus.SUNK);
+        //assertTrue(board_a.attack(7,'H').getResult() == AtackStatus.SUNK);
 
-        assertTrue(board.attack(1,'C').getResult() == AtackStatus.HIT);
-        assertTrue(board.attack(2,'C').getResult() == AtackStatus.SURRENDER);    */  //To check if the last ship sunk the player lose
+
     }
 }
