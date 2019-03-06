56869ceac31d67d3f9857ff72862c02c6b4325e4
ssl654534335 <40868502+ssl654534335@users.noreply.github.com>
2019-03-05 23:25:43 -0800

    Add files via upload

--- src/main/java/controllers/PlacementGameAction.java
+++ src/main/java/controllers/PlacementGameAction.java
@@ -10,6 +10,7 @@ public class PlacementGameAction {
     @JsonProperty private int x;
     @JsonProperty private char y;
     @JsonProperty private boolean isVertical;
+    @JsonProperty private boolean isSubmerged;
 
     public Game getGame() {
         return game;
@@ -29,5 +30,9 @@ public class PlacementGameAction {
 
     public boolean isVertical() {
         return isVertical;
+    }
+
+    public boolean isSubmerged() {
+        return isSubmerged;
     }
 }
